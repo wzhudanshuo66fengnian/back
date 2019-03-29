b7502beffbd873e9dae0c66c7cf989c53ddab083
wzhudanshuo66fengnian <shuoshuoshuolove@163.com>
2019-03-29 11:18:48 +0800

    增加一个商品类

---
+++ src/main/java/com/neuedu/pojo/Product.java
@@ -0,0 +1,11 @@
+package com.neuedu.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by 12708 on 2019/3/29.
+ */
+public class Product {
+    private String name;
+    private BigDecimal price;
+}
